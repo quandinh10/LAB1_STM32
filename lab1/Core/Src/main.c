07cc09b41efc05cea436df54a6f6651e49a516bb
quandinh10 <dinhquan1003@gmail.com>
2023-09-14 15:34:07 +0700

    add schematic ex6

--- lab1/Core/Src/main.c
+++ lab1/Core/Src/main.c
@@ -131,19 +131,6 @@ int main(void)
   /* USER CODE BEGIN 2 */
   int counter=0;
 
-  HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, SET);
-  HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, SET);
-  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, SET);
-  HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, SET);
-  HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, SET);
-  HAL_GPIO_WritePin(LED5_GPIO_Port, LED5_Pin, SET);
-  HAL_GPIO_WritePin(LED6_GPIO_Port, LED6_Pin, SET);
-  HAL_GPIO_WritePin(LED7_GPIO_Port, LED7_Pin, SET);
-  HAL_GPIO_WritePin(LED8_GPIO_Port, LED8_Pin, SET);
-  HAL_GPIO_WritePin(LED9_GPIO_Port, LED9_Pin, SET);
-  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
-  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, SET);
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -202,7 +189,8 @@ int main(void)
 		  HAL_GPIO_WritePin(LED10_GPIO_Port, LED10_Pin, SET);
 		  HAL_GPIO_WritePin(LED11_GPIO_Port, LED11_Pin, RESET);
 	  }
-	  counter++;
+	  if (counter==11) counter=0;
+	  else counter++;
 	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
@@ -261,7 +249,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LED0_Pin|LED1_Pin|LED2_Pin|LED3_Pin
                           |LED4_Pin|LED5_Pin|LED6_Pin|LED7_Pin
-                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin, GPIO_PIN_RESET);
+                          |LED8_Pin|LED9_Pin|LED10_Pin|LED11_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pins : LED0_Pin LED1_Pin LED2_Pin LED3_Pin
                            LED4_Pin LED5_Pin LED6_Pin LED7_Pin
